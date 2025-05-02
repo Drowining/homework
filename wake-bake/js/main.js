42b3c451b0f9f64c609f9d774fee82e819fafa58
Max <maks-litvinov20@mail.ru>
2025-05-02 22:28:39 +0700

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,11 +1,10 @@
+// ========================burger-menu==================================================
 (function () {
     document.addEventListener('click', burgerInit);
 
     function burgerInit(e) {
         const burgerIcon = e.target.closest('.burger-icon');
         const burgerNavLink = e.target.closest('.nav__link');
-
-
         if (burgerIcon) {
             document.body.classList.toggle('body--opened-menu');
             return;
@@ -13,10 +12,27 @@
         if (document.documentElement.clientWidth > 900) {
             return
         }
-
-
         if (burgerNavLink) {
             document.body.classList.remove('body--opened-menu');
         }
     }
 })();
+
+
+// =================================modal==================================
+
+const modal = document.querySelector('.modal')
+const imgGiftButton = document.querySelector('.about__img-button')
+const modalCancel = document.querySelector('.modal__cancel')
+
+imgGiftButton.addEventListener('click', () => modal.classList.add('modal--opened'))
+modalCancel.addEventListener('click', (event) => {
+    event.preventDefault()
+    modal.classList.remove('modal--opened')
+})
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.classList.remove('modal--opened');
+    }
+});
