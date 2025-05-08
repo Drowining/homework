306c062e9fc4be4cf4d2dbb7e3c7448a3a724de3
Max <maks-litvinov20@mail.ru>
2025-05-08 19:30:19 +0700

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -159,3 +159,7 @@ new Swiper('.testimonials__slider', {
         },
     }
 });
+// ===================input-mask-tel==================================
+const tellInputs = document.querySelectorAll('input[type="tel"]')
+const im = new Inputmask('+7 (999) 999-99-99')
+im.mask(tellInputs)
